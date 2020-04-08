d0bda5a2bfc433123eaf4d95afaf02fc74a326fc
matejskubic <839965+matejskubic@users.noreply.github.com>
2020-04-08 14:44:01 +0200

    template for parameter splating

--- Update-BcPackages/Run-Update-BcPackages.ps1
+++ Update-BcPackages/Run-Update-BcPackages.ps1
@@ -13,3 +13,26 @@ $SkipVerification = Get-VstsInput -Name SkipVerification -AsBool
 $ForceAppDataUpgrade = Get-VstsInput -Name ForceAppDataUpgrade -AsBool
 
 .\Update-BcPackages.ps1 -ServerInstance $ServerInstance -AppPackagesFolder $AppPackagesFolder -PackagesToInstall $PackagesToInstall -SyncMode $SyncMode -SkipVerification:$SkipVerification -ForceAppDataUpgrade:$ForceAppDataUpgrade
+
+
+
+
+
+
+
+### plan with generic parameter matching
+$inputArguments = @{}
+
+$command = Get-Command .\Update-BcPackages.ps1
+$command.Parameters.GetEnumerator() | ForEach-Object {
+    $paramValue = [System.Management.Automation.ParameterMetadata]$_.Value
+    $paramName = $paramValue.Name
+    if ($paramValue.SwitchParameter) {
+        $inputArguments.$paramName = Get-VstsInput -Name $paramName -AsBool
+    }
+    else {
+        $inputArguments.$paramName = Get-VstsInput -Name $paramName
+    }
+}
+$inputArguments # prints arguments
+#.\Update-BcPackages.ps1 @inputArguments
